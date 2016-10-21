294c4b8fa42118b9b11a3819bd48acdffaed0df8
jacklin <1272442383@qq.com>
2016-10-21 16:58:32 +0800

    intial pro by yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demoByAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the demoByAngularApp
+ */
+angular.module('demoByAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
